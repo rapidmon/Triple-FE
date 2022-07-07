8218940e063f6b417377ff7eb336596af6eba75f
Lim Heelae <ehhwll@hanmail.net>
2022-07-08 03:52:33 +0900

    :bug: clearInterval가 안 되던 버그 수정

--- src/hook/useCountup.js
+++ src/hook/useCountup.js
@@ -1,23 +1,27 @@
 import { useEffect, useRef } from "react";
 import { easeOutExpo } from "../lib/util";
-import { START_NUMBER, DURATION_TIME, FRAME_RATE } from "../lib/constants";
+import {
+  START_NUMBER,
+  DURATION_TIME,
+  FRAME_RATE,
+  PROGRESS_DONE,
+} from "../lib/constants";
 
-const CountUp = (num) => {
+const CountUp = (CountUpNumber) => {
   const domRef = useRef();
   const totalFrame = Math.round(DURATION_TIME / FRAME_RATE);
-
   useEffect(() => {
     const { current } = domRef;
     let currentNumber = START_NUMBER;
     const counter = setInterval(() => {
       const progress = easeOutExpo(currentNumber / totalFrame);
       currentNumber++;
-      current.innerText = Math.round(num * progress);
-      if (progress === true) {
+      current.innerText = Math.round(CountUpNumber * progress);
+      if (progress === PROGRESS_DONE) {
         clearInterval(counter);
       }
     }, FRAME_RATE);
-  }, [num, totalFrame]);
+  }, [CountUpNumber, totalFrame]);
   return {
     ref: domRef,
   };
