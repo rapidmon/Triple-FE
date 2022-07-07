98063e7550d630d0c7c000c759abe50f8e2fde72
Lim Heelae <ehhwll@hanmail.net>
2022-07-07 22:36:16 +0900

    :lipstick: Title index component 완료

---
+++ src/components/Title/index.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+import Content from "./content";
+
+const ContentWrapper = styled.div`
+  margin-left: 623px;
+  padding-top: 150px;
+`;
+
+function Title() {
+  return (
+    <ContentWrapper>
+      <Content num="700" strong="만 명" text="의 여행자"></Content>
+      <Content num="100" strong="만 개" text="의 여행 리뷰"></Content>
+      <Content num="470" strong="만 개" text="의 여행 일정"></Content>
+    </ContentWrapper>
+  );
+}
+
+export default Title;
