c643db0ede9ed419fa1520f1b5b9690a045c23de
Lim Heelae <ehhwll@hanmail.net>
2022-07-07 15:11:26 +0900

    :lipstick: Award component 생성

---
+++ src/components/Awards/award.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import playstore from "../../assets/play-store2x.png";
+import appstore from "../../assets/badge-apple4x.png";
+
+const Item = styled.div`
+  background-size: 54px 54px;
+  height: 54px;
+  padding: 5px 0px 5px 62px;
+  font-size: 14px;
+  line-height: 22px;
+  margin-right: 39px;
+  display: inline-block;
+  font-family: sans-serif;
+  background-image: ${(props) =>
+    props.id === "google" ? `url(${playstore})` : `url(${appstore})`};
+  background-position: left top;
+  background-repeat: no-repeat;
+  color: var(--color-gray800);
+  font-weight: bold;
+`;
+
+function Award({ text1, text2, id }) {
+  return (
+    <Item id={id}>
+      {text1}
+      <br />
+      {text2}
+    </Item>
+  );
+}
+
+export default Award;
